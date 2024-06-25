b99b0d842f0a64c11425f0d72ae9488147126e2d
genaro gonzalez <genarrogg@gmail.com>
2024-06-25 18:48:06 -0400

    implementacion de algolia

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,28 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "strapi-algolia": {
+    enabled: true,
+    config: {
+      apiKey: env("ALGOLIA_ADMIN_KEY"),
+      applicationId: env("ALGOLIA_APP_ID"),
+      contentTypes: [
+        {
+          name: "api::trabajo.trabajo",
+          populate: {
+            path: "titulo, descripcion",
+          },
+          hideFields: [
+            "publishedAt",
+            "createdAt",
+            "updatedAt",
+            "downloader",
+            "cedulaAutor",
+            "apellidoAutor",
+            "nombreAutor",
+            "emailAutor",
+            "id",
+          ],
+        },
+      ],
+    },
+  },
+});
