44e57fa555621c4d37a3af0db465988beb0a49a1
Genaro Gonzalez <genarrogg@gmail.com>
2024-07-25 12:53:01 -0400

    bibli

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,32 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "*"],
+          "media-src": ["'self'", "data:", "blob:", "*"],
+          "frame-src": [
+            "http://localhost:*",
+            "self",
+            "sandbox.embed.apollographql.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
